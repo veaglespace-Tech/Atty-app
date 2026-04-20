9afb011688a36af8331af182debdc85f2336285a
AkshaySingare <singareakshay937@gmail.com>
2026-04-20 23:55:56 +0530

    added .env content

--- server/config/hostinger.js
+++ server/config/hostinger.js
@@ -1,4 +1,4 @@
-const CLIENT_BASE_URL = "http://atty.veaglespace.com";
-const API_BASE_URL = "http://atty.veaglespace.com/api";
+const CLIENT_BASE_URL = "https://atty.veaglespace.com";
+const API_BASE_URL = "https://atty.veaglespace.com/api";
 
 module.exports = { CLIENT_BASE_URL, API_BASE_URL };
