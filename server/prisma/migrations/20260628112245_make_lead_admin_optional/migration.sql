b2854e901e0d9c68b1ed8a54a43747a135578df5
Akshay Singare <158290336+AkshaySingare@users.noreply.github.com>
2026-07-17 11:42:36 +0530

    Delete server directory

--- server/prisma/migrations/20260628112245_make_lead_admin_optional/migration.sql
+++
@@ -1,6 +0,0 @@
--- DropIndex
-DROP INDEX `registration_lead_adminEmail_key` ON `registration_lead`;
-
--- AlterTable
-ALTER TABLE `registration_lead` MODIFY `adminName` VARCHAR(191) NULL,
-    MODIFY `adminEmail` VARCHAR(191) NULL;
--- server/prisma/migrations/20260629000000_add_referral_partner/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `isReferralPartner` BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN `partnerReferralCode` VARCHAR(191) NULL;
-
--- CreateIndex
-CREATE UNIQUE INDEX `User_partnerReferralCode_key` ON `user`(`partnerReferralCode`);
